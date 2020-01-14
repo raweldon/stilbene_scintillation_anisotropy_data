6409a30d7093703374c3b0faddcd6f477ca2e3dd
raweldon <r.a.weldon.jr@gmail.com>
2020-01-14 18:38:57 -0500

    data files with script to view orientation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # stilbene_scintillation_anisotropy_data
 Measured and smoothed light output and pulse shape parameter anisotropy for recoil proton energies between 0.56 and 10 MeV 
+
+## Text files    
+The measured and smoothed data are listed in ascii files.  Each file contains the light output or pulse shape parameter values for a given recoil proton energy along with the direction of the given parameter with respect to the crystal axes.
+
+### Contents
+Each text file is composed of 4 columns.  Column 1 is the light ouptput (MeVee) or pulse shape parameter (unitless).  Columns 2 - 4 are the x, y, and z coordinates, respectively.
---
+++ view_data.py
@@ -0,0 +1,65 @@
+
+import glob
+import pandas
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import os
+from matplotlib import cm
+
+
+# data file directories
+cwd = os.getcwd()
+measured_lo_path = cwd + '/measured_data/light_output/'
+measured_psp_path = cwd + '/measured_data/pulse_shape_parameter/'
+smoothed_lo_path = cwd + '/smoothed_data/light_output/'
+smoothe_psp_path = cwd + '/smoothed_data/pulse_shape_parameter/' 
+
+files = glob.glob(smoothed_lo_path + '*')
+
+for fi in files:
+    with open(fi, 'r') as f:
+        df = pandas.read_csv(f, delimiter=r'\s+', names=['param', 'x', 'y', 'z', 'crystal'])
+
+    if 'measured' in fi:
+        # plot x, y, z for with crystal 1 and 2 labeled
+        df_crys1 = df.loc[df['crystal'] == 'crystal_1']
+        df_crys2 = df.loc[df['crystal'] == 'crystal_2']
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(df_crys1['x'].values, df_crys1['y'].values, df_crys1['z'].values, c='r', label='bvert')
+        ax.scatter(df_crys2['x'].values, df_crys2['y'].values, df_crys2['z'].values, c='b', label='cpvert')
+        ax.set_xlim([-1.1,1.1])
+        ax.set_ylim([-1.1,1.1])
+        ax.set_zlim([-1.1,1.1])
+        ax.set_xlabel('a')
+        ax.set_ylabel('b')
+        ax.set_zlabel('c\'')
+        ax.set_aspect('equal')
+        title = fi.split('\\')
+        ax.set_title(title[-1])
+        plt.tight_layout()
+        plt.legend()
+
+    # plot param values - no interpolation
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    p = ax.scatter(df['x'].values, df['y'].values, df['z'].values, c=df['param'].values, cmap='viridis')
+    ax.set_xlim([-1.1,1.1])
+    ax.set_ylim([-1.1,1.1])
+    ax.set_zlim([-1.1,1.1])
+    ax.set_xlabel('a')
+    ax.set_ylabel('b')
+    ax.set_zlabel('c\'')
+    ax.set_aspect('equal')
+    title = fi.split('\\')
+    ax.set_title(title[-1])
+    plt.colorbar(p)
+    plt.tight_layout()
+    plt.show()
+
+ 
+
+
+    
+
